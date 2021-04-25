9c6f1a635043bd3ea1a4930b5f778af035f1c06f
zjh6666 <974813758@qq.com>
2021-04-25 15:30:21 +0800

    vite.config changes

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+// vite.config.js # or vite.config.ts
+
+module.exports = {
+    alias: {
+        // 键必须以斜线开始和结束
+        '/@/': path.resolve(__dirname, './src')
+    },
+    // 是否自动在浏览器打开
+    open: true,
+    // 是否开启 https
+    https: false,
+    // 服务端渲染
+    ssr: false,
+    /**
+     * 在生产中服务时的基本公共路径。
+     * @default '/'
+     */
+    base: './',
+    /**
+     * 与“根”相关的目录，构建输出将放在其中。如果目录存在，它将在构建之前被删除。
+     * @default 'dist'
+     */
+    outDir: 'dist',
+    // 反向代理
+    proxy: {
+        // '/api': {
+        //     target: '后台URL',
+        //     changeOrigin: true,
+        //     rewrite: path => path.replace(/^\/api/, '')
+        // }
+    }
+}
