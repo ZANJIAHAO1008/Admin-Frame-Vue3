6b68d55188f2c510078d011d4bcfd5f01e11abe3
ZAN <974813758@qq.com>
2021-11-30 18:34:07 +0800

    Adding Version Logs

---
+++ src/assets/js/versionLog.js
@@ -0,0 +1,19 @@
+//记录版本日志
+export const versionLog = [
+    {
+        version: "V1.0.1",
+        description:
+            ['vue版本升级至3.2.22.',
+                '项目中添加vueuse库.',
+                '新增生成二维码、监测电脑信息组件.'
+            ],
+        releaseDate: "2021/11/24"
+    },
+    {
+        version: "V1.0.2",
+        description:
+            ['新增卡片列表.'
+            ],
+        releaseDate: "2021/11/26"
+    },
+];
